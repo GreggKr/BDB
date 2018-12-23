8c98cd53b2ab91af3831a4c674a2b1f9757aa379
Grzegorz <greggkrzykowski@gmail.com>
2018-12-23 15:17:26 -0600

    restrict offline user

--- src/main/kotlin/me/greggkr/bdb/db/Data.kt
+++ src/main/kotlin/me/greggkr/bdb/db/Data.kt
@@ -1,9 +1,11 @@
 package me.greggkr.bdb.db
 
-import me.greggkr.bdb.data
 import me.greggkr.bdb.logging.LogType
 import me.greggkr.bdb.util.toHex
-import net.dv8tion.jda.core.entities.*
+import net.dv8tion.jda.core.entities.Guild
+import net.dv8tion.jda.core.entities.Role
+import net.dv8tion.jda.core.entities.TextChannel
+import net.dv8tion.jda.core.entities.User
 import java.awt.Color
 
 class Data(private val db: Database) {
@@ -14,11 +16,7 @@ class Data(private val db: Database) {
 
     private val owners = listOf(
             184041169796333568, // Gregg
-            184733130123378688, // Landon
-            216596910792769537, // Yosh
-            131392299514068992, // Hand
-            199374658200403969, // Michael (Plowayy)
-            346755297714372609 // Luminiscental
+            184733130123378688 // Landon
     )
 
     fun isOwner(user: User) = owners.contains(user.idLong)
@@ -104,15 +102,15 @@ class Data(private val db: Database) {
         return map
     }
 
-   fun isNRARestricted(user: User): Boolean {
-       return user.idLong in db.getNRARestricted()
+   fun isNRARestricted(id: Long): Boolean {
+       return id in db.getNRARestricted()
    }
 
-    fun nraRestrictUser(user: User) {
-        db.addNRARestricted(user.idLong)
+    fun nraRestrictUser(id: Long) {
+        db.addNRARestricted(id)
     }
 
-    fun nraUnrestrictUser(user: User) {
-        db.removeNRARestricted(user.idLong)
+    fun nraUnrestrictUser(id: Long) {
+        db.removeNRARestricted(id)
     }
 }
--- src/main/kotlin/me/greggkr/bdb/nra/RestrictEventListener.kt
+++ src/main/kotlin/me/greggkr/bdb/nra/RestrictEventListener.kt
@@ -6,7 +6,7 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter
 
 class RestrictEventListener : ListenerAdapter() {
     override fun onGuildMemberJoin(e: GuildMemberJoinEvent) {
-        if (data.isNRARestricted(e.user)) {
+        if (data.isNRARestricted(e.user.idLong)) {
             e.guild.controller.addRolesToMember(e.member, e.guild.getRoleById(RESTRICTED_ROLE_ID)).queue()
         }
     }
